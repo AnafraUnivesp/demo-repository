be0f87730f57d2560800f10445b4c8169cb4064a
analaurafra <analaura.francisco@gmail.com>
2024-10-11 23:28:55 -0300

    inclusao de database e deploy via azure

---
+++ equilibresemgluten/projeto_cad/app_cad_clientes/admin.py
@@ -0,0 +1,13 @@
+
+# Register your models here.
+
+#from django.contrib import admin
+
+# Register your models here.
+#from .models import  id_cliente  
+
+#admin.site.register( id_cliente)
+#admin.site.register(id_observacao)
+
+
+#Verificar com Thais
